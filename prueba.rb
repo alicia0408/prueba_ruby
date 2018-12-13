24f409d114516bf11961382ba11a1389464c7202
Alicia <tucorreo@mail.com>
2018-12-12 21:18:08 -0300

    working menu

--- prueba.rb
+++ prueba.rb
@@ -1,52 +1,47 @@
+ 
+def  welcome_message
+  puts "#################################################################"
+  puts "Bienvenido al sitema de gestion de estudiantes, por favor selecciona una opción"
+  puts "#################################################################"
+  puts "\n"
 
-class Menu 
-    attr_reader :exit 
-    def  initialize
-        @exit = false 
-    end
-    def  show_menu
+  show_menu
+end
+
+ def  show_menu        
         puts "1) Nombre y promedio del alumno"
         puts "2) Inasistencias totales"
         puts "3)Alumnos aprobados "
-        puts "4)salir"
+        puts  "4)salir"
         puts "Opcion:"
+       options
+ end
+
+
+  def options
+      option = gets.chomp.to_i       
+      case option
+      when 1 then 
+        name_average
+       when 2 then 
+       total_absences  
+       when 3 then 
+        approved_students
+       when 4 then
+           puts "Hasta luego"
+        else
+        puts "Opción  invalida, coloque una opción correcta."
+        show_menu
+      end
     end
-    def options
-        option = gets.chomp.to_i
-        
-        case option
-        when 1 then 
-            name_average
-        when 2 then 
-         total_absences
-        when 3 then 
-            approved_students
-        when 4
-            @exit = true 
-        
-        when  -1 .. 0
-            puts "opcion invalida"
-           
-        when  5.. 100
-       puts "opcion invalida"
-          end
-     
-         def name_average
+      
+  def total_absences
 
-            end
 
-            def total_absences
+  end
 
-            end
+  def approved_students
 
-            def approved_students
-
-            end
-    end
-end
-
-menu = Menu.new
-until menu.exit 
-    menu.show_menu
-    menu.options
-end
+  end
+   
+   welcome_message
